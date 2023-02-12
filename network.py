fbbaa529abc9cbc0bb31c89b6851501cf45bf95f
brianbianchi <bianchibrianj@gmail.com>
2023-02-12 00:50:40 -0500

    desc

--- network.py
+++ network.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import json
 
+# scrape network traffic
+
 desired_capabilities = DesiredCapabilities.CHROME
 desired_capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
 
